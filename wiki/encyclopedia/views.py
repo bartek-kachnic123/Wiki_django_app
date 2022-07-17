8d3a3538f9f84697ebebb264d7f57534a35b1fb8
bartek-kachnic123 <bartek.kachnic123@gmail.com>
2022-07-17 17:17:46 +0200

    Created entry_page view and added in urlpatterns

--- wiki/encyclopedia/views.py
+++ wiki/encyclopedia/views.py
@@ -1,10 +1,24 @@
+from django.http import HttpResponseNotFound
 from django.shortcuts import render
 
 from . import util
 
-
+# Main site
 def index(request):
     return render(request, "encyclopedia/index.html", {
         "entries": util.list_entries()
     })
 
+
+def entry_page(request, title):
+    
+    # Get content data
+    content = util.get_entry(title)
+    
+    # Checks if entry exists
+    if content is None:
+        return HttpResponseNotFound("Not found")
+
+    return render(request, "encyclopedia/content.html", { 
+        "title": title.capitalize(), "content": content
+        })
