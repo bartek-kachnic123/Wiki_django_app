5b7eb59feb69d0f938668008828694eec2da14e7
bartek-kachnic123 <bartek.kachnic123@gmail.com>
2022-07-18 11:50:24 +0200

    Added search view and updated  urlpatterns

--- wiki/encyclopedia/views.py
+++ wiki/encyclopedia/views.py
@@ -1,5 +1,6 @@
-from django.http import HttpResponseNotFound
+from django.http import HttpResponseNotFound, HttpResponseRedirect
 from django.shortcuts import render
+from django.urls import reverse
 
 from . import util
 
@@ -11,7 +12,7 @@ def index(request):
 
 
 def entry_page(request, title):
-    
+
     # Get content data
     content = util.get_entry(title)
     
@@ -22,3 +23,33 @@ def entry_page(request, title):
     return render(request, "encyclopedia/content.html", { 
         "title": title.capitalize(), "content": content
         })
+
+
+def search(request):
+
+    # if query empty, redirect to home page
+    if "q" not in request.GET:
+        return HttpResponseRedirect(reverse('index'))
+
+    # Get search data
+    search_query = request.GET.get("q").strip()
+
+    # if entry is empty, redirect to home page
+    if len(search_query) == 0:
+        return HttpResponseRedirect(reverse('index'))
+
+    # Get entries
+    entries = util.list_entries()
+    
+    # if entry found,redirect to entry page
+    if search_query in entries:
+        return HttpResponseRedirect(reverse('entry_page', args=(search_query,)))
+
+    # Redirect filtred data
+    search_query = search_query.casefold()
+    return render(request, "encyclopedia/search.html", {"entries": list(filter(lambda entry: search_query in entry.casefold(), entries))})
+
+        
+        
+
+
