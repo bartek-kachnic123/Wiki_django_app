b7526ffc75f5eecdf34de1b02bd0a0dc59a491ef
bartek-kachnic123 <bartek.kachnic123@gmail.com>
2022-07-19 13:29:21 +0200

    Added PageForm

---
+++ wiki/encyclopedia/forms.py
@@ -0,0 +1,7 @@
+from typing import Text
+from django.forms import Form, CharField, Textarea, TextInput
+
+
+class PageForm(Form):
+    title = CharField(widget=TextInput(attrs={'class': 'form-control', 'style': 'margin-bottom: 10px' }), label="Title", max_length=50)
+    content = CharField(widget=Textarea(attrs={'class': 'form-control', 'style': 'margin-bottom: 10px'}), label="Content", max_length=1000)
